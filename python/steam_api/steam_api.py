da9ee7f3fb3044494b8ee014c044d56c5db20d7b
Joe Enser <joseph.enser@mlb.com>
2023-03-01 11:51:53 -0500

    Created method to get friends list

--- python/steam_api/steam_api.py
+++ python/steam_api/steam_api.py
@@ -6,7 +6,7 @@ import requests
 class SteamApi:
     """Class to interact with Steam API"""
 
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         """Instantiate SteamApi class
 
         Args:
@@ -34,6 +34,33 @@ class SteamApi:
         querystring = {
             "key": self.api_key,
             "steamids": steamid
+        }
+
+        payload = ""
+        response = requests.get(
+            url=url,
+            data=payload,
+            params=querystring
+        )
+
+        return json.loads(response.text)
+
+    def get_friend_list(self, steamid: str) -> list:        
+        """Get friends list
+
+        Args:
+            steamid (str): Steam player id
+
+        Returns:
+            list: List dictionaries of player summary data
+
+        """
+
+        url = f"{self.base_url}/ISteamUser/GetFriendList/v0001/"
+        querystring = {
+            "key": self.api_key,
+            "steamid": steamid,
+            "relationship": "friend"
         }
 
         payload = ""
--- python/steam_api/steam_api_test.py
+++ python/steam_api/steam_api_test.py
@@ -4,13 +4,14 @@ from os import environ
 from steam_api import SteamApi
 
 steam_api_key = environ.get("STEAM_API_KEY")
+steamid = "76561198056269216"
 
 sa = SteamApi(
     api_key=steam_api_key
 )
 
 player_summary_response = sa.get_player_summaries(
-    steamid="76561198056269216"
+    steamid=steamid
 )
 
 # Write player summary results to csv file
@@ -20,3 +21,14 @@ with open("/Users/joseph.enser/Downloads/steam_player_summaries.csv", "w") as f:
         if i == 0:
             writer.writerow(player.keys())
         writer.writerow(player.values())
+
+friend_list_response = sa.get_friend_list(
+    steamid=steamid
+)
+
+with open("/Users/joseph.enser/Downloads/steam_friends_list.csv", "w") as f:
+    writer = csv.writer(f)
+    for i, friend in enumerate(friend_list_response.get("friendslist").get("friends")):
+        if i == 0:
+            writer.writerow(friend.keys())
+        writer.writerow(friend.values())
