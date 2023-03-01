3f77c4d53e557db3c90ebe6e9e794a89c1a14d7d
Joe Enser <joseph.enser@mlb.com>
2023-03-01 12:01:11 -0500

    Updated to use pathlib

--- python/steam_api/steam_api_test.py
+++ python/steam_api/steam_api_test.py
@@ -1,5 +1,6 @@
 import csv
 from os import environ
+from pathlib import Path
 
 from steam_api import SteamApi
 
@@ -14,8 +15,10 @@ player_summary_response = sa.get_player_summaries(
     steamid=steamid
 )
 
+downloads_dir = Path.home() / "Downloads"
+
 # Write player summary results to csv file
-with open("/Users/joseph.enser/Downloads/steam_player_summaries.csv", "w") as f:
+with open(f"{downloads_dir}/steam_player_summaries.csv", "w") as f:
     writer = csv.writer(f)
     for i, player in enumerate(player_summary_response.get("response").get("players")):
         if i == 0:
@@ -26,7 +29,7 @@ friend_list_response = sa.get_friend_list(
     steamid=steamid
 )
 
-with open("/Users/joseph.enser/Downloads/steam_friends_list.csv", "w") as f:
+with open(f"{downloads_dir}/steam_friends_list.csv", "w") as f:
     writer = csv.writer(f)
     for i, friend in enumerate(friend_list_response.get("friendslist").get("friends")):
         if i == 0:
