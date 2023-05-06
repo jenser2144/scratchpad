d01d16540c314e08f042b9629f94c1813c3702ce
Joe Enser <joeyenser@gmail.com>
2023-05-05 21:00:40 -0400

    Updated model

--- dbt/src/test_dbt_project/models/staging/book_db_stg.sql
+++ dbt/src/test_dbt_project/models/staging/book_db_stg.sql
@@ -8,24 +8,23 @@
 WITH book_db AS (
 
     SELECT
-        TRIM(title) AS title,
-        TRIM(toggl_title) AS toggl_title,
-        TRIM(author) AS author,
-        TRIM(published) AS published,
-        TRIM(genre_1) genre_1,
-        TRIM(genre_2) AS genre_2,
-        TRIM(genre_3) AS genre_3,
-        TRIM(type) AS type,
-        pages::integer,
-        TRIM(series) AS series,
-        book_num::integer,
-        start_dt,
-        end_dt,
+        TRIM(BOTH FROM title) AS title,
+        TRIM(BOTH FROM toggl_title) AS toggl_title,
+        TRIM(BOTH FROM author) AS author,
+        TRIM(BOTH FROM published) AS published,
+        TRIM(BOTH FROM genre_1) genre_1,
+        TRIM(BOTH FROM genre_2) AS genre_2,
+        TRIM(BOTH FROM genre_3) AS genre_3,
+        TRIM(BOTH FROM type) AS type,
+        pages::int,
+        TRIM(BOTH FROM series) AS series,
+        book_num::float,
+        start_dt::date,
+        end_dt::date,
         rating_goodreads::float,
-        my_rating::integer,
-        TRIM(read_status) AS read_status,
-        TRIM(version_read) AS version_read,
-        date_added
+        my_rating::float,
+        TRIM(BOTH FROM read_status) AS read_status,
+        TRIM(BOTH FROM version_read) AS version_read
     FROM
         {{ source("raw", "book_db") }}
 )
